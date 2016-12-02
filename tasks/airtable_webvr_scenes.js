53e01da0fb99aebaf31258f49ec472a0b5551a05
Christopher Van Wiemeersch <hearcomestreble@gmail.com>
2016-12-01 22:37:55 -0800

    update Firebase in real-time based on Airtable changes! 🎈 💨 🎉 💯

--- tasks/airtable_webvr_scenes.js
+++ tasks/airtable_webvr_scenes.js
@@ -1,6 +1,7 @@
 var fs = require('fs');
 var path = require('path');
 
+var firebase = require('firebase');
 var request = require('request-promise');
 
 var logger = require('../logger')(__filename);
@@ -12,6 +13,23 @@ var AIRTABLE_WEBVR_SCENES_PATH = path.resolve(
 var AIRTABLE_WEBVR_SCENES_RELATIVE_TO_ROOT_PATH = path.relative(
   '..', AIRTABLE_WEBVR_SCENES_PATH
 );
+
+var settings = module.exports.settings = {
+  firebase: {
+    credentials: {
+      apiKey: 'AIzaSyCfMvL2DsldNeLy3LMC6gFrMD_HAFOLT-M',
+      authDomain: 'webvr-6345b.firebaseapp.com',
+      databaseURL: 'https://webvr-6345b.firebaseio.com',
+      storageBucket: 'webvr-6345b.appspot.com',
+      messagingSenderId: '689640619063'
+    },
+    ref: 'webvrrocks/webvr_scenes'
+  }
+};
+
+var firebaseApp = firebase.initializeApp(settings.firebase.credentials);
+
+var firebaseRef = firebaseApp.database().ref(settings.firebase.ref);
 
 module.exports.options = {
   maxAttempts: 50,
@@ -27,11 +45,20 @@ module.exports.run = function () {
       'Authorization': 'Bearer keyMJq1gSRuwMTZ8r'
     }
   }).then(function (data) {
-    logger.info('Writing to %s', AIRTABLE_WEBVR_SCENES_RELATIVE_TO_ROOT_PATH);
+    var dataObj = null;
+    try {
+      dataObj = JSON.parse(data);
+    } catch (e) {
+      logger.warn('Could not parse data as JSON', data);
+    }
 
-    // TODO: Push to Firebase!
+    logger.info('Updating Firebase database "%s"', settings.firebase.ref);
+    if (dataObj) {
+      firebaseRef.set(dataObj);
+    }
 
-    fs.createWriteStream(AIRTABLE_WEBVR_SCENES_PATH).write(data);
+    // logger.info('Writing to "%s"', AIRTABLE_WEBVR_SCENES_RELATIVE_TO_ROOT_PATH);
+    // fs.createWriteStream(AIRTABLE_WEBVR_SCENES_PATH).write(data);
   }).catch(function (err) {
     // API call failed.
     logger.error(err);
